0ac340415673e52d0c138941c62b682f0039d83a
백승진 <bsj1048@naver.com>
2017-04-27 22:55:46 +0900

    change function evaluate with Rule class

--- src/main/java/com/edu/cnu/poker/Evaluator.java
+++ src/main/java/com/edu/cnu/poker/Evaluator.java
@@ -9,25 +9,25 @@ import java.util.Map;
  * Created by cse on 2017-04-17.
  */
 public class Evaluator {
-    public String evaluate(List<Card> cardList) {
+
+    Rule onePairRule = null;
+    Rule twoPairRule = null;
+    Rule tripleRule = null;
+    Rule flushRule = null;
+
+    public Rule evaluate(List<Card> cardList) {
 
         boolean flush_flag=false;
-        Map<Suit, Integer> tempMap = new HashMap<Suit, Integer>();
+        Map<Suit, Integer> tempMap1 = new HashMap<Suit, Integer>();
         Map<Integer, Integer> tempMap2 = new HashMap<Integer, Integer>();
 
         for (Card card : cardList) {
-            if (tempMap.containsKey(card.getSuit())) {
-                Integer count = tempMap.get(card.getSuit());
+            if (tempMap1.containsKey(card.getSuit())) {
+                Integer count = tempMap1.get(card.getSuit());
                 count = new Integer(count.intValue() + 1);
-                tempMap.put(card.getSuit(), count);
+                tempMap1.put(card.getSuit(), count);
             } else {
-                tempMap.put(card.getSuit(), new Integer(1));
-            }
-        }
-
-        for (Suit key : tempMap.keySet()) {
-            if (tempMap.get(key) == 5) {
-                flush_flag = true;
+                tempMap1.put(card.getSuit(), new Integer(1));
             }
         }
         for (Card card : cardList) {
@@ -35,44 +35,68 @@ public class Evaluator {
                 Integer count = tempMap2.get(card.getRank());
                 count = new Integer(count.intValue() + 1);
                 tempMap2.put(card.getRank(), count);
-
             }else {
                 tempMap2.put(card.getRank(), new Integer(1));
             }
         }
-        for (Integer key : tempMap2.keySet()) {
+        boolean flush = false;
+        for (Suit key : tempMap1.keySet()) {
+            if (tempMap1.get(key) == 5) {
+                flushRule = new Rule(EnumRule.FLUSH, key);
+                flush = true;
+            }
+        }
+        boolean triple = false;
+        for (int key : tempMap2.keySet()) {
+            if (tempMap2.get(key) == 3) {
+                tripleRule = new Rule(EnumRule.TRIPLE, key);
+                triple = true;
+            }
+        }
+        int pairCount = 0;
+        for (int key : tempMap2.keySet()) {
             if (tempMap2.get(key) == 4) {
-                return "FOUR_CARD";
-            }else if (tempMap2.get(key) == 2) {
-                tempMap2.remove(key);
-                for (Integer key2 : tempMap2.keySet()){
-                    if(tempMap2.get(key2) == 2){
-                        return "TWO_PAIR";
-                    }else if(tempMap2.get(key2) == 3){
-                        return "FULL_HOUSE";
-                    }else return "ONE_PAIR";
-                }
-            }else if (tempMap2.get(key) == 3) {
-                return "TRIPLE";
+                Rule fourCardRule = new Rule(EnumRule.FOUR_CARD, key);
+                return fourCardRule;
             }
-        }
 
-        Collections.sort(cardList);
-
-        for(int i = 0; i < cardList.size() ; i++ ) {
-            if(i == cardList.size() -1 ){
-                if(flush_flag){
-                    return "STRAIGHT_FLUSH";
+            if (tempMap2.get(key) == 2) {
+                switch (pairCount) {
+                    case 0:
+                        onePairRule = new Rule(EnumRule.ONE_PAIR, key);
+                        twoPairRule = new Rule(EnumRule.TWO_PAIR, key);
+                        break;
+                    case 1:
+                        if(twoPairRule.getFirst() < key) {
+                            twoPairRule.setSecond(twoPairRule.getFirst());
+                            twoPairRule.setFirst(key);
+                            break;
+                        }
+                        else
+                            twoPairRule.setSecond(key);
+                        break;
                 }
-                return "STRAIGHT" ;
+                if (triple) {
+                    Rule fullHouseRule = new Rule(EnumRule.FULL_HOUSE, tripleRule.getFirst());
+                    fullHouseRule.setSecond(key);
+                    return fullHouseRule;
+                }
+                pairCount++;
             }
-            if(cardList.get(i).compareTo(cardList.get(i+1)) != -1 ){
-                break;
+            if (triple)
+                return tripleRule;
+        }
+        if(!flush) {
+            switch (pairCount) {
+                case 0:
+                    break;
+                case 1:
+                    return onePairRule;
+                case 2:
+                    return twoPairRule;
             }
         }
-        if(flush_flag){
-            return "FLUSH";
-        }
-        return "NOTHING";
+        Rule nothingRule = new Rule(EnumRule.NOTHING);
+        return nothingRule;
     }
 }
