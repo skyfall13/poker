a5c601caba54f183b0de3c97ae511544491219d6
OpalCat <noblesse3539@daum.net>
2017-04-28 02:03:03 +0900

    RuleTest_getSecond를비교

--- src/test/java/com/edu/cnu/poker/RuleTest.java
+++ src/test/java/com/edu/cnu/poker/RuleTest.java
@@ -2,6 +2,9 @@ package com.edu.cnu.poker;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertTrue;
@@ -35,5 +38,30 @@ public class RuleTest {
         int result = myRule.compareTo(yourRule);
         System.out.println(result);
         assertTrue( result< 0 );
+    }
+    @Test
+    public void getSecond를비교() {
+        Evaluator evaluator1 = new Evaluator();
+        Evaluator evaluator2 = new Evaluator();
+        List<Card> cardList1 = Arrays.asList(
+                new Card(1,Suit.CLUBS),
+                new Card(1,Suit.DIAMONDS),
+                new Card(2,Suit.DIAMONDS),
+                new Card(7,Suit.HEARTS),
+                new Card(7,Suit.SPADES)
+        );
+        List<Card> cardList2 = Arrays.asList(
+                new Card(1,Suit.HEARTS),
+                new Card(1,Suit.SPADES),
+                new Card(2,Suit.HEARTS),
+                new Card(7,Suit.DIAMONDS),
+                new Card(7,Suit.CLUBS)
+        );
+        Rule myRule = evaluator1.evaluate(cardList1);
+        Rule yourRule = evaluator2.evaluate(cardList2);
+        int result = myRule.compareTo(yourRule);
+        System.out.println(result);
+        assertTrue( result< 0 );
+
     }
 }
